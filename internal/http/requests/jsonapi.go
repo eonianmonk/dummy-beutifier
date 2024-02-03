7f2e14e39e963adb0907b8402d667e13e396a779
eonianmonk <berezhnenko.dmitry@gmail.com>
2024-02-03 17:28:47 +0200

    fix typo

--- internal/http/requests/jsonapi.go
+++ internal/http/requests/jsonapi.go
@@ -6,7 +6,7 @@ type Article struct {
 	Id        int        `jsonapi:"primary,post"`
 	Title     string     `jsonapi:"attr,title"`
 	Body      string     `jsonapi:"attr,body"`
-	CreatedAt time.Time  `jsonapi:"attr, created_at"`
+	CreatedAt time.Time  `jsonapi:"attr,created_at"`
 	Author    string     `jsonapi:"attr,author"`
 	Comments  []*Comment `jsonapi:"relation,comments"`
 }
@@ -15,5 +15,5 @@ type Comment struct {
 	Id        int       `jsonapi:"primary,comment"`
 	Author    string    `jsonapi:"attr,author"`
 	Body      string    `jsonapi:"attr,body"`
-	CreatedAt time.Time `jsonapi:"attr, created_at"`
+	CreatedAt time.Time `jsonapi:"attr,created_at"`
 }
